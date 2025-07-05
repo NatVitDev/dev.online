775d2a4a655bedd61e7ca07b7a4df3aaba892079
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2025-07-05 19:34:46 +0300

    add Logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import LocaleSwitcher from "../LocaleSwitcher/LocaleSwitcher";
 import Nav from "../Nav/Nav";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
     <>
+      <Logo />
       <Nav />
       <LocaleSwitcher />
     </>
