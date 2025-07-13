bb10fdee0ac97ad4b01d8c4630dc9d9b1fbc7d09
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2025-07-13 22:06:27 +0300

    Warning

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.cache = false;
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
